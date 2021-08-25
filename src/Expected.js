6818ddb6736fe97fc577ac55a93c703ab9b3a424
Mark Borchard <mark@basewinner.com>
2021-08-24 17:10:47 -0700

    added published date to Xstandings

--- src/Expected.js
+++ src/Expected.js
@@ -1,12 +1,15 @@
 import React from "react";
 import expected_standings from "./pictures/expected_standings.jpg";
 import {ExpectedTable} from './components/ExpectedTable'
+import "./Expected.css";
+
 
 function Expected() {
   return (
     <div>
       <h1 className="standings">MLB Expected Standings</h1>
       <ExpectedTable />
+      <h5 className="left">Published Tuesday, Agust 24th @ 7:00 AM ET</h5>
       <h3>Chart Key</h3>
     </div>
   );
