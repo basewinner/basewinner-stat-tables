8496a88ad2bccf85053afd97b089361d4b36e095
Mark Borchard <mark@basewinner.com>
2021-09-03 18:11:31 -0700

    Updated X Standings for September 3

--- src/Expected.js
+++ src/Expected.js
@@ -9,7 +9,7 @@ function Expected() {
     <div>
       <h1 className="standings">MLB Expected Standings</h1>
       <ExpectedTable />
-      <h5 className="left">Published Thursday, September 2nd @ 7:00 AM ET</h5>
+      <h5 className="left">Published Friday, September 3rd @ 7:00 AM ET</h5>
       <h3>Chart Key</h3>
     </div>
   );
